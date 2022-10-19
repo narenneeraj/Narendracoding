7e758f57da2155a1c629b45dd888d04789fa033f
Narendra_github <111925017+narenneeraj@users.noreply.github.com>
2022-10-19 13:08:04 +1100

    Updated_File_share.tf

--- fs.tf
+++ fs.tf
@@ -1,12 +1,12 @@
 resource azurerm_resource_group "resource_group"{
-name = "rg1212"
+name = "rg_name"
 location = "eastus"
 }
 
 resource "azurerm_storage_account" "storage_account"{
-name = "storage1213"
+name = "storage_name"
 location = "eastus"
-resource_group_name = "rg1212"
+resource_group_name = "rg_name"
 account_tier = "Standard"
 account_replication_type = "LRS"
 }
@@ -14,7 +14,7 @@ provider "azurerm" {
     features {}
 }
 resource "azurerm_storage_share" "file_share" {
-  name                 = "fileshares1213"
-  storage_account_name = "storage1213"
+  name                 = "fileshare_name"
+  storage_account_name = "storage_name"
   quota                = 50
 }
